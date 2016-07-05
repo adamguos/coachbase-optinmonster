042538682ce50cbf062529d758c6bc119012aec7
xtqmc <adamexcaliburguo@gmail.com>
2016-07-05 16:02:14 +0800

    Switched to express and body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var http = require('http');
+var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
@@ -7,7 +8,44 @@ var authKey =
 		'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';
 var sendGridListId = 69751;
 
-var server = http.createServer(function(req, res) {
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.post('/', function(req, res) {
+	console.log(req.body);
+
+	var str = JSON.stringify([{
+		'email': req.body.email
+	}]);
+
+	request.post({
+		headers: {'Authorization': 'Bearer ' + authKey},
+		url: 'https://api.sendgrid.com/v3/contactdb/recipients',
+		body: str
+	}, function(err, res, body) {
+		console.log('Status code ' + res.statusCode);
+		console.log('SendGrid response: ' + body);
+
+		var obj = JSON.parse(body);
+		var id = obj.persisted_recipients;
+
+		request.post({
+			headers: {'Authorization': 'Bearer ' + authKey},
+			url: 'https://api.sendgrid.com/v3/contactdb/lists/' + sendGridListId + '/recipients/' + id,
+		}, function(err, res, body) {
+			console.log('Status code ' + res.statusCode);
+			console.log('SendGrid response: ' + body + '\n');
+		});
+	});
+
+	res.redirect(req.body.url);
+})
+
+/*var server = http.createServer(app, function(req, res) {
 	if (req.method == 'POST') {
 		var body = '';
 
@@ -15,7 +53,7 @@ var server = http.createServer(function(req, res) {
 			body += data;
 		});
 		req.on('end', function() {
-			console.log('data: ' + body);
+			console.log(body);
 
 			var obj = JSON.parse(body);
 			var str = JSON.stringify([{
@@ -51,4 +89,6 @@ var server = http.createServer(function(req, res) {
 	}
 });
 
-server.listen(port);
+server.listen(port);*/
+
+var server = app.listen(port);
